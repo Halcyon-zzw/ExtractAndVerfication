ca39ae8a1459c74aa4e9e23e3147ef07523ab8ee
GILDATA\zhuzw <zhuzw@gildata.com>
2019-10-08 17:11:42 +0800

    Optional操作

---
+++ java8学习/src/main/java/lambda/LocalDateTimeLearn.java
@@ -0,0 +1,11 @@
+package lambda;
+
+/**
+ * TODO
+ *
+ * @Author: zhuzw
+ * @Date: 2019/10/8 16:42
+ * @Version: 1.0
+ */
+public class LocalDateTimeLearn {
+}
---
+++ java8学习/src/main/java/lambda/OptionalLearn.java
@@ -0,0 +1,11 @@
+package lambda;
+
+/**
+ * TODO
+ *
+ * @Author: zhuzw
+ * @Date: 2019/10/8 15:50
+ * @Version: 1.0
+ */
+public class OptionalLearn {
+}
