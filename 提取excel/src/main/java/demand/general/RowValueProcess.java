1168947eb1b81f4abf8e870cec98b9956c742242
GILDATA\zhuzw <zhuzw@gildata.com>
2019-10-14 14:18:42 +0800

    提取：         1、修改getArticle()方法，添加删除无效数据的处理方法         2、自定义字符串处理类新增删除括号方法、删除字符串mark前一个标识和后一个标识之间的内容、删除Mark及之后的内容、删除mark及之前的内容

--- 提取excel/src/main/java/demand/general/RowValueProcess.java
+++ 提取excel/src/main/java/demand/general/RowValueProcess.java
@@ -1,5 +1,7 @@
 package demand.general;
 
+import util.StringsUtilCustomize;
+
 /**
  * 行数据处理
  * 使用直接处理，需要计算重写getLabel方法
@@ -10,9 +12,16 @@ package demand.general;
  */
 public class RowValueProcess {
 
+    /**
+     * 字符串的最小长度
+     */
+    private final int lessLength = 20;
+
+    static int count = 0;
 
     /**
      * 默认直接使用标签；需计算重写该方法
+     *
      * @param labels
      * @return
      */
@@ -22,24 +31,37 @@ public class RowValueProcess {
 
     public String getArticle(String title, String content) {
         /**
-         * 截取字符的长度
+         * 截取字符的长度，拼接时需要一个字符
          */
-        int articleLength = 10000;
+        int articleLength = 512;
+        //处理特殊字符
         content = dealString(content);
+        //删除无效内容
+        content = deleteInvalid(content);
         String article = title + " " + content;
+
+        //删除括号
+        article = StringsUtilCustomize.substringByDeleteBrackets(article);
 
         //判断长度是否超过
         if (article.length() > articleLength) {
+            String resultArticle = "";
             //截取内容
-            article = article.substring(0, articleLength);
+            resultArticle = article.substring(0, 256);
+            resultArticle += article.substring(article.length() - 256, article.length());
+
+            return resultArticle;
         }
         return article;
+
     }
+
 
     /**
      * 提取一行数据
-     * @param labels 用于计算标签的参数
-     * @param title 标题
+     *
+     * @param labels  用于计算标签的参数
+     * @param title   标题
      * @param content 内容
      * @return
      */
@@ -58,10 +80,102 @@ public class RowValueProcess {
     protected String dealString(String tempString) {
 
         return tempString
-                .replaceAll("\b","")
-                .replaceAll("\f","")
-                .replaceAll("\n","")
-                .replaceAll("\r","")
-                .replaceAll("\t","");
+                .replaceAll("\b", " ")
+                .replaceAll("\f", " ")
+                .replaceAll("\n", " ")
+                .replaceAll("\r", " ")
+                .replaceAll("\t", " ");
     }
+
+    /**
+     * 删除无效内容
+     * <p>
+     * bug:
+     * 存在content为null的情况
+     *
+     * @param content
+     * @return
+     */
+    public String deleteInvalid(String content) {
+
+        //省略号字符串
+        String elipsis1String = "............";
+        String elipsis2String = "------------";
+        //“存在风险字符串”
+        String riskString = "注意投资风险";
+
+        String tempContent = content;
+        //删除开头20字符内的xxx讯
+        int newsIndex = content.indexOf("讯");
+        if (newsIndex < 20) {
+            content = StringsUtilCustomize.substringByDeleteBefore(content, "讯");
+        }
+
+        //删除开头100字符内"有限公司"之前内容
+        int comptyIndex = content.indexOf("有限公司");
+        if (comptyIndex < 50 && !content.endsWith("有限公司") && content.length() - comptyIndex - 4 > lessLength) {
+            //位置在50以内，且不已“有限公司结尾” 且删除后长度大于lessLength
+            content = StringsUtilCustomize.substringByDeleteBefore(content, "有限公司");
+        }
+
+        //删除目录内容及之前的内容
+        if (!content.endsWith(".............") && !content.endsWith("-------------")) {
+            //文章不以"..........."或"----------"结尾
+
+            //省略号位置
+            int elipsisIndex_1 = content.lastIndexOf(elipsis1String);
+            int elipsisIndex_2 = content.lastIndexOf(elipsis2String);
+            if (content.length() - elipsisIndex_1 - elipsis1String.length() > lessLength && content.length() - elipsisIndex_2 - elipsis2String.length() > lessLength) {
+                //省略号位置在文末最小长度以外字符以外
+
+                content = StringsUtilCustomize.substringByDeleteBeforeLast(content, ".............");
+                content = StringsUtilCustomize.substringByDeleteBeforeLast(content, "-------------");
+            }
+
+
+        }
+
+        //删除 “特此公告”及之后内容
+        content = StringsUtilCustomize.substringByDeleteAfterLast(content, "特此公告");
+
+        //删除声明内容 =>  删除“虚假记载、误导性陈述”上一个"。"至下一个"。"的内容
+        content = StringsUtilCustomize.substringByDelete(content, "虚假记载、误导性陈述", "。", "。");
+
+        //删除“备份文件” 前一个"、"和下一个"。"之间的内容
+        content = StringsUtilCustomize.substringByDelete(content, "备份文件", "、", "。");
+
+        //删除“注意投资风险”
+        int riskIndex = content.lastIndexOf(riskString);
+        if (content.length() - riskIndex - riskString.length() > lessLength) {
+            //替换后字符串长度大于lessLength
+
+            content = StringsUtilCustomize.substringByDeleteAll(content, "注意投资风险", "。", "。");
+            content = StringsUtilCustomize.substringByDeleteAll(content, "注意投资风险", "。", "!");
+        } else {
+            content = content.replaceAll(riskString, "");
+        }
+
+
+        //删除"资料显示"、"报告显示"
+        content = content.replace("资料显示", "");
+        content = content.replace("报告显示", "");
+
+        //删除最后结尾非标点的内容
+        if (content.length() > 0) {
+            String lastString = content.substring(content.length() - 1, content.length());
+            String endMark = "，。？.?”！!";
+            boolean endOfMark = endMark.contains(lastString);
+            if (!endOfMark) {
+                int lastProdIndex = content.lastIndexOf("。");
+                if (-1 != lastProdIndex) {
+                    //不止一句话
+                    content = content.substring(0, lastProdIndex + 1);
+                }
+            }
+        }
+
+        return content;
+    }
+
+
 }
--- 提取excel/src/main/java/util/StringsUtilCustomize.java
+++ 提取excel/src/main/java/util/StringsUtilCustomize.java
@@ -36,4 +36,136 @@ public class StringsUtilCustomize {
         }
         return false;
     }
+
+    /**
+     * 删除字符串中所有的括号，不能删除嵌套括号
+     * @param str
+     * @return
+     */
+    public static String substringByDeleteBrackets(String str) {
+        //存在左右括号不一致问题，同意中英文括号
+        str = str.replaceAll("\\（", "(");
+        str = str.replaceAll("\\）", ")");
+
+        str = str.replaceAll("\\（[^\\（^\\）]*\\）", "");
+        str = str.replaceAll("\\([^\\(^\\)]*\\)", "");
+
+        return str;
+    }
+
+
+    /**
+     * 删除所有出现的地方
+     *
+     * @param str
+     * @param markString 标识字符串，用来查询字符串
+     * @param beforeMark 标识字符串之前的mark（保留）
+     * @param afterMark 标识字符串之后的mark（删除）
+     * @return
+     */
+    public static String substringByDeleteAll(String str, String markString, String beforeMark, String afterMark) {
+        String tempStr = substringByDelete(str, markString, afterMark, beforeMark);
+        while (tempStr.length() < str.length()) {
+            str = tempStr;
+            tempStr = substringByDelete(str, markString, afterMark, beforeMark);
+
+        }
+        return tempStr;
+    }
+
+    /**
+     * 通过标识字符串，删除标识字符串前后的字符串（第一次匹配的字符串）
+     *
+     * @param str 需要处理的字符串
+     * @param beforeMark 标识字符串前的标识
+     * @param afterMark 标识字符串后的标识
+     * @param markString 标识字符串
+     * @return
+     */
+    public static String substringByDelete(String str, String markString, String beforeMark, String afterMark) {
+        int markIndex = str.indexOf(markString);
+        if (-1 != markIndex) {
+            //存在 markString 内容,
+            //markString 前面的内容
+            String beforeStr = str.substring(0, markIndex);
+            //markString后面的内容
+            String afterStr = str.substring(markIndex + markString.length(), str.length());
+
+            //前半部 beforeMark 的Index
+            int beforeMarkIndex = beforeStr.lastIndexOf(beforeMark);
+            //后半部 afterMark 的Index
+            int afterMarkIndex = afterStr.indexOf(afterMark);
+
+            //截除标识间的内容
+            if (-1 != beforeMarkIndex) {
+                //beforemark存在
+                beforeStr = beforeStr.substring(0, beforeMarkIndex + 1);
+            } else {
+                //不存在则截除所有
+                beforeStr = "";
+            }
+
+            //处理afterMark未找到情况
+            if (-1 != afterMarkIndex) {
+                //存在afterMark，afterStr = afterIndex之后的内容
+                afterStr = afterStr.substring(afterMarkIndex + 1, afterStr.length());
+            }else {
+                //不存在afterMark，afterStr = markString之后的内容
+                afterStr = afterStr;
+            }
+
+
+            //拼接
+            str = beforeStr + afterStr;
+        }
+        return str;
+    }
+
+
+    /**
+     * 删除markString（最后一个匹配）及其之后的内容
+     *
+     * @param str 待处理的字符串
+     * @param markString 标识字符串
+     * @return
+     */
+    public static String substringByDeleteAfterLast(String str, String markString) {
+        int markIndex = str.lastIndexOf(markString);
+        if (-1 != markIndex) {
+            //存在 markString 内容
+            str = str.substring(0, markIndex);
+        }
+        return str;
+    }
+
+    /**
+     * 删除markString（第一次匹配）及其之前的内容
+     *
+     * @param str 待处理的字符串
+     * @param markString 标识字符串
+     * @return
+     */
+    public static String substringByDeleteBefore(String str, String markString) {
+        int markIndex = str.indexOf(markString);
+        if (-1 != markIndex) {
+            str = str.substring(markIndex + markString.length(), str.length());
+        }
+        return str;
+    }
+
+
+    /**
+     * 删除markString（最后一次匹配）及其之前的内容
+     *
+     * @param str 待处理的字符串
+     * @param markString 标识字符串
+     * @return
+     */
+    public static String substringByDeleteBeforeLast(String str, String markString) {
+        int markIndex = str.lastIndexOf(markString);
+        if (-1 != markIndex) {
+            str = str.substring(markIndex + markString.length(), str.length());
+        }
+        return str;
+    }
 }
