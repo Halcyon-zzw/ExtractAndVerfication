a62d8edc5d3fa9a4748eb4b561f553626dc50ed0
GILDATA\zhuzw <zhuzw@gildata.com>
2019-10-31 09:42:54 +0800

    一些对象创建放在类初始化时

--- 测试模型/src/main/java/request/ClassificationRequest.java
+++ 测试模型/src/main/java/request/ClassificationRequest.java
@@ -2,8 +2,7 @@ package request;
 
 import bert.BertRequest;
 import bert.single.BertResultSingle;
-import config.ApplicationProperties;
-import config.PropertiesFactory;
+import lombok.Data;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,22 +15,29 @@ import java.nio.charset.StandardCharsets;
  * @Date: 2019/9/3 15:01
  * @Version: 1.0
  */
+@Data
 public abstract class ClassificationRequest<T> {
     int num = 0;
     /**
      * 获取请求地址
      */
-    String url = TestUrlFactory.getTestUrl("emotion");
+    private String url = TestUrlFactory.getTestUrl("emotion");
 
     /**
      * 请求参数
      */
     BertRequest bertRequest;
 
+    private RestTemplate restTemplate = new RestTemplate(); // 使用HttpClient，支持GZIP
+
     /**
      * 请求响应类型
      */
     private Class<T> responseType;
+
+    public ClassificationRequest() {
+
+    }
 
     public ClassificationRequest(BertRequest bertRequest, Class<T> responseType) {
         this.bertRequest = bertRequest;
@@ -41,17 +47,21 @@ public abstract class ClassificationRequest<T> {
 
     /**
      * post请求
+     *
      * @return
      */
     public Object postForObject() {
-        RestTemplate restTemplate = new RestTemplate(); // 使用HttpClient，支持GZIP
+
+
         restTemplate.getMessageConverters().set(1,
                 new StringHttpMessageConverter(StandardCharsets.UTF_8)); // 支持中文编码
+
         return restTemplate.postForObject(url, bertRequest, responseType);
     }
 
     /**
      * 从响应结果中获取结果
+     *
      * @return
      */
     public abstract BertResultSingle getResult();
--- 测试模型/src/main/java/request/SingleClassificationRequest.java
+++ 测试模型/src/main/java/request/SingleClassificationRequest.java
@@ -3,6 +3,7 @@ package request;
 import bert.BertRequest;
 import bert.single.BertResultSingle;
 import bert.single.BertSingleResponse;
+import lombok.Data;
 
 /**
  * 单分类请求
@@ -11,7 +12,16 @@ import bert.single.BertSingleResponse;
  * @Date: 2019/9/3 15:48
  * @Version: 1.0
  */
+@Data
 public class SingleClassificationRequest extends ClassificationRequest {
+
+    private BertResultSingle bertResult = new BertResultSingle();
+
+    public SingleClassificationRequest() {
+        super();
+
+    }
+
 
     public SingleClassificationRequest(BertRequest bertRequest, Class responseType) {
         super(bertRequest, responseType);
@@ -19,10 +29,12 @@ public class SingleClassificationRequest extends ClassificationRequest {
 
     @Override
     public BertResultSingle getResult() {
-        BertResultSingle bertResult = new BertResultSingle();
-        BertSingleResponse bertSingleResponse = (BertSingleResponse)super.postForObject();
+
+        BertSingleResponse bertSingleResponse = (BertSingleResponse) super.postForObject();
         bertResult.setScore(bertSingleResponse.getResult().get(0).getScore().get(0).floatValue());
         bertResult.setLabel(bertSingleResponse.getResult().get(0).getPred_label().get(0));
+
+
         return bertResult;
     }
 }
